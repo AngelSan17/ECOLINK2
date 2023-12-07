14c3ee0b730420763e65288bcba9064c674b9632
AngelSan17 <112904127+AngelSan17@users.noreply.github.com>
2023-12-07 00:51:19 -0600

    Add files via upload
    presentacion  final app

--- app/src/main/java/me/sandria/ecolink/CommunityFragment.kt
+++ app/src/main/java/me/sandria/ecolink/CommunityFragment.kt
@@ -1,7 +1,5 @@
 package me.sandria.ecolink
 
-import Evento
-import EventosAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,55 +10,21 @@ import androidx.lifecycle.ViewModelProvider
 // Otras importaciones...
 
 
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
-
-class CommunityFragment : Fragment(), EventosAdapter.OnEventoClickListener {
-
-    private lateinit var viewModel: EventosViewModel
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var eventosAdapter: EventosAdapter
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        viewModel = ViewModelProvider(this).get(EventosViewModel::class.java)
-        // Continúa con la configuración de tu RecyclerView y otros elementos de la UI
-        recyclerView = view.findViewById(R.id.events_recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-
-        val eventos = obtenerEventosDePrueba()
-        eventosAdapter = EventosAdapter(eventos, this)
-        recyclerView.adapter = eventosAdapter
-    }
+class CommunityFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.activity_community_fragment, container, false)
-        recyclerView = view.findViewById(R.id.events_recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-
-        val eventos = obtenerEventosDePrueba()
-        eventosAdapter = EventosAdapter(eventos, this)
-        recyclerView.adapter = eventosAdapter
-
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_community_fragment, container, false)
     }
 
-    override fun onEventoClicked(evento: Evento) {
-        viewModel.seleccionarEvento(evento)
-        // Aquí, en lugar de reemplazar el fragmento, simplemente actualizas el ViewModel.
-        // El fragmento de eventos observará estos cambios.
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Aquí puedes inicializar elementos de tu UI o lógica relacionada con la vista del fragmento
     }
 
-    private fun obtenerEventosDePrueba(): List<Evento> {
-        return listOf(
-            Evento("Limpieza de Playa", "Limpieza", "10:00 AM", "Juan Pérez", "url_imagen_1"),
-            Evento("Recogida en el Parque", "Reciclaje", "2:00 PM", "Ana Gómez", "url_imagen_2"),
-            // Más eventos...
-        )
-    }
+    // Si no tienes más lógica que implementar, no necesitas añadir nada más aquí.
 }
--- app/src/main/java/me/sandria/ecolink/EventosAdapter.kt
+++ app/src/main/java/me/sandria/ecolink/EventosAdapter.kt
@@ -1,3 +1,6 @@
+package me.sandria.ecolink
+
+import Evento
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -5,15 +8,38 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
-import me.sandria.ecolink.R
 
 class EventosAdapter(
-    private val eventosList: List<Evento>,
+    private var eventosList: MutableList<Evento>,
     private val listener: OnEventoClickListener
 ) : RecyclerView.Adapter<EventosAdapter.EventoViewHolder>() {
 
+    // Se usa para el filtro, para tener siempre la lista original completa
+    private var eventosListFull: List<Evento> = ArrayList(eventosList)
+
     interface OnEventoClickListener {
         fun onEventoClicked(evento: Evento)
+    }
+
+    inner class EventoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val nombreEvento: TextView = itemView.findViewById(R.id.nombre_evento)
+        private val tipoEvento: TextView = itemView.findViewById(R.id.tipo_evento)
+        private val horaEvento: TextView = itemView.findViewById(R.id.hora_evento)
+        private val encargadoEvento: TextView = itemView.findViewById(R.id.encargado_evento)
+        private val imagenEvento: ImageView = itemView.findViewById(R.id.imagen_evento)
+
+        fun bind(evento: Evento) {
+            nombreEvento.text = evento.nombre
+            tipoEvento.text = evento.tipo
+            horaEvento.text = evento.hora
+            encargadoEvento.text = evento.encargado
+            if (evento.imagenUrl.isNotEmpty()) {
+                Picasso.get().load(evento.imagenUrl).into(imagenEvento)
+            } else {
+                imagenEvento.setImageResource(R.mipmap.ic_launcher) // Asume que tienes este recurso en tu proyecto
+            }
+            itemView.setOnClickListener { listener.onEventoClicked(evento) }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventoViewHolder {
@@ -28,24 +54,24 @@ class EventosAdapter(
 
     override fun getItemCount() = eventosList.size
 
-    inner class EventoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val nombreEvento: TextView = itemView.findViewById(R.id.nombre_evento)
-        private val tipoEvento: TextView = itemView.findViewById(R.id.tipo_evento)
-        private val horaEvento: TextView = itemView.findViewById(R.id.hora_evento)
-        private val encargadoEvento: TextView = itemView.findViewById(R.id.encargado_evento)
-        private val imagenEvento: ImageView = itemView.findViewById(R.id.imagen_evento)
-
-        fun bind(evento: Evento) {
-            nombreEvento.text = evento.nombre
-            tipoEvento.text = evento.tipo
-            horaEvento.text = evento.hora
-            encargadoEvento.text = evento.encargado
-
-            if(evento.imagenUrl.isNotEmpty()) {
-                Picasso.get().load(evento.imagenUrl).into(imagenEvento)
+    fun filter(query: String) {
+        val filteredList = if (query.isEmpty()) {
+            eventosListFull
+        } else {
+            eventosListFull.filter {
+                it.nombre.contains(query, ignoreCase = true) || it.tipo.contains(query, ignoreCase = true)
+                // Añade más condiciones de filtrado si es necesario
             }
-
-            itemView.setOnClickListener { listener.onEventoClicked(evento) }
         }
+        eventosList.clear()
+        eventosList.addAll(filteredList)
+        notifyDataSetChanged()
+    }
+
+    fun updateEventosList(eventos: List<Evento>) {
+        eventosListFull = ArrayList(eventos) // Actualiza la lista completa para el filtro
+        this.eventosList.clear()
+        this.eventosList.addAll(eventos)
+        notifyDataSetChanged()
     }
 }
