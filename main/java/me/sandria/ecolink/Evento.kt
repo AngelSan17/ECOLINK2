00ca551b342854b45ab763620d25da5696c1198c
AngelSan17 <112904127+AngelSan17@users.noreply.github.com>
2023-12-07 01:39:59 -0600

    Delete main directory

--- main/java/me/sandria/ecolink/Evento.kt
+++
@@ -1,7 +0,0 @@
-data class Evento(
-    val nombre: String,
-    val tipo: String,
-    val hora: String,
-    val encargado: String,
-    val imagenUrl: String // URL de la imagen en Firebase Storage
-)
